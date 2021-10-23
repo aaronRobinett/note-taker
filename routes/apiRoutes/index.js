5b847b17c8efa3f3b30ada50b61f98b4a2948467
Aaron <aaronrobinett@gmail.com>
2021-10-23 11:45:31 -0700

    finished all routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,21 @@
 const { notes } = require('../../db/db.json');
 const router = require('express').Router();
-
+const { createNewNote, validateNote } = require('../../notes.js');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const apiRoutes = require('./routes/apiRoutes');
-//const htmlRoutes = require('./routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -9,20 +9,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', apiRoutes);
-//app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`server now on port ${PORT}!`);
 });
-
-router.post('/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
-  
-    if (!validateNote(req.body)) {
-      res.status(400).send('The note is not properly formatted.');
-    } else {
-      const note = createNewNote(req.body, notes);
-      res.json(note);
-    }
-  });
